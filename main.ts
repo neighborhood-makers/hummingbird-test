f806d99e89dc3237ab769d2726b6d26fc449ccab
neighborhood-makers <53877723+neighborhood-makers@users.noreply.github.com>
2023-01-21 17:35:47 -0500

    blink slowly

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 hummingbird.startHummingbird()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    hummingbird.setLED(ThreePort.One, 50)
+    basic.pause(1000)
+    hummingbird.setLED(ThreePort.One, 0)
+    basic.pause(1000)
 })
